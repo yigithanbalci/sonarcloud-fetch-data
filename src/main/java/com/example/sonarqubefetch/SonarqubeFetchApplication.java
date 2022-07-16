ce5bb1ab8a4b07479df32df7c0cd4ccb76076887
Yigithan Kadir BALCI <yigithanbalci@gmail.com>
2022-07-16 21:52:23 +0300

    Initial Commit

---
+++ src/main/java/com/example/sonarqubefetch/SonarqubeFetchApplication.java
@@ -0,0 +1,12 @@
+package com.example.sonarqubefetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeFetchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SonarqubeFetchApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/sonarqubefetch/SonarqubeFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarqubefetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
